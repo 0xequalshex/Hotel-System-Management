c8e05a5c0e2dddda80813748363830228e7edd34
wglsl <azzozalmehmadi@gmail.com>
2026-05-08 22:16:52 +0300

    Update Person.java

--- Model/Person.java
+++ Model/Person.java
@@ -1,5 +1,46 @@
 package models;
 
-public class Person {
+public abstract class Person {
+	protected int id;
+	protected String name;
+	protected String email;
+	
+	public Person(int id, String name, String email) { 
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		
+		
+	}
+	
+	
+	
+	public int getId() { 
+		return this.id;	
+	}
+	public String getName() { 
+		return this.name;
+	}
+	public String getEmail() { 
+		return this.email;
+	}
+	
+	
+	public void setId(int id) { 
+		this.id = id; 
+	}
+	public void setName(String name) { 
+		this.name = name;
+	}
+	public void setEmail(String email) { 
+		this.email = email;
+	}
+	
+	public abstract void print();
 
+		
+	
+
+	
+	
 }
