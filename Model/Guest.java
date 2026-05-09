b054a52e2da4f7fa53651d5c6345c72a08b51419
Abdulrahman Alghamdi <abdulrahmanalghama@gmail.com>
2026-05-09 16:26:34 +0300

    Update Guest.java

--- Model/Guest.java
+++ Model/Guest.java
@@ -7,15 +7,14 @@ public class Guest extends Person {
 		super(id, name, email);
 		this.discount = discount;
 	}
+	
 	public Guest() { 
 		super();
 	}
-	public int getDiscount() { 
-		return this.discount;
-	}
-	public void setDiscount(int discount) { 
-		this.discount = discount;
-	}
+	
+	public void setDiscount(int discount) {this.discount = discount;}
+	public int getDiscount() {return this.discount;}
+	
 	@Override
 	public void print() { 
 		System.out.println("   ID    :" + this.id);
@@ -24,13 +23,11 @@ public class Guest extends Person {
 		System.out.println("   Discount :" + this.discount + "%");
 	}
 	public void print(String label) { 
-		System.out.println(label);
+		System.out.println("  [" + label + "]");
 		this.print();
 	}
 	
 	public String toCsv() { 
 		return this.id + "," + this.name + "," + this.email + "," + this.discount;
-		
-		
 	}
 }
